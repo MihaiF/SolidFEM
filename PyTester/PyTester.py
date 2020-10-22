Fixed nonlinear config; cantilever tests; gradient descent and scipy minimization

PyTester/PyTester.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
-
 from Simulator import Simulator
+from utils import read_tetfile
 
 def plot_nodes(nodes):
     # plot the mesh   
@@ -26,51 +26,154 @@ print('Hello!')
 # 2 EXPLICIT,
 # 3 IMPLICIT,
 
-# prepaper the FEM tetrahedron
-config = {
-    "young": 66000,
-    "poisson": 0.4,
-    "simtype": 2
-}
+def test_tetrahedron():
+    config = {
+        "young": 66000,
+        "poisson": 0.45,
+        "simtype": 2,
+        "substeps": 1,
+        'maxiter': 100,
+    }
 
-hw = 0.05
-tets = np.array([[0, 1, 2, 3]], dtype=np.int)
-nodes = np.array([[0, -hw, hw], [0, -hw, -hw], [0, hw, hw], [2 * hw, -hw, hw]], dtype=np.double)
-fixed_nodes = np.array([0, 1, 2])
+    # tetrahedron
+    hw = 0.05
+    tets = np.array([[0, 1, 2, 3]], dtype=np.int)
+    nodes = np.array([[0, -hw, hw], [0, -hw, -hw], [0, hw, hw], [2 * hw, -hw, hw]], dtype=np.double)
+    fixed_nodes = np.array([0, 1, 2])
 
-# create the simulator
-fem = psf.NonlinearFEM(tets, nodes, fixed_nodes, config)
+    # create the simulator
+    fem = psf.NonlinearFEM(tets, nodes, fixed_nodes, config)
 
-dt = 0.016
-numSteps = 2
-fem.save_to_vtk('tetrahedron0.vtk')
-for i in range(0, numSteps):
+    dt = 0.016
+    numSteps = 2
+    fem.save_to_vtk('tetrahedron0.vtk')
+    for i in range(0, numSteps):
+        fem.step(dt)
+        nodes1 = fem.get_nodes()
+        print(nodes1)
+        fem.save_to_vtk('tetrahedron{0}.vtk'.format(i+1))
+
+    # Python simulator
+    sim = Simulator(nodes, tets, fixed_nodes, config)
+    nodes3 = sim.step_explicit()
+    nodes3 = sim.step_explicit()
+    print(nodes3)
+
+def test_tetrahedron_static():
+    config = {
+        "young": 66000,
+        "poisson": 0.45,
+        "simtype": 0,
+        "substeps": 1,
+        'maxiter': 100,
+    }
+
+    # tetrahedron
+    hw = 0.05
+    tets = np.array([[0, 1, 2, 3]], dtype=np.int)
+    nodes = np.array([[0, -hw, hw], [0, -hw, -hw], [0, hw, hw], [2 * hw, -hw, hw]], dtype=np.double)
+    fixed_nodes = np.array([0, 1, 2])
+
+    # create the simulator
+    fem = psf.NonlinearFEM(tets, nodes, fixed_nodes, config)
+
+    dt = 0.016
+    numSteps = 2
     fem.step(dt)
     nodes1 = fem.get_nodes()
     print(nodes1)
-    fem.save_to_vtk('tetrahedron{0}.vtk'.format(i+1))
+    fem.save_to_vtk('tetrahedron.vtk')
 
-# Python simulator
-#sim = Simulator(nodes, tets, fixed_nodes)
-#sim.step_explicit()
-#nodes3 = sim.step_explicit()
-#print(nodes3)
+    # Python simulator
+    sim = Simulator(nodes, tets, fixed_nodes, config)
+    #nodes3 = sim.solve_grad_desc(2e-3, 100, 1e-12)
+    nodes3 = sim.solve_scipy()
+    print(nodes3)
 
-N, T, F = psf.load_from_xml('hammerbot.xml')
-print(N)
-hammerbot = Simulator(N, T.flatten(), F)
-hammerbot.dt = 0.016 / 200
-for i in range(0, 10):
-    print('.', end='')
-    nodes4 = hammerbot.step()
-print(nodes4)
-plot_nodes(nodes4)
+def test_cantilever():
+    config = {
+        "young": 66000,
+        "poisson": 0.45,
+        "simtype": 2,
+        "substeps": 10,
+        'maxiter': 100,
+    }
 
-# create the simulator from file
-#fem = psf.NonlinearFEM('hammerbot.xml')
-#fem.step()
-#nodes = fem.get_nodes()
-#fem.save_to_vtk("hammerbot.vtk")
+    # load the box
+    verts, indices = read_tetfile('box2.tet')
+    fixed2 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    num_steps = 10
+
+    # C++ simulator
+    sim2 = psf.NonlinearFEM(indices, verts, fixed2, config)
+    nodes2 = sim2.get_nodes()
+    sim2.save_to_vtk('cantilever0.vtk')    
+    for iter in range(0, num_steps):
+        sim2.step()
+        nodes2 = sim2.get_nodes()
+        sim2.save_to_vtk('cantilever{0}.vtk'.format(iter + 1))
+    print(nodes2[-1])
+    plot_nodes(nodes2)
+
+    # Python simulator
+    sim2 = Simulator(verts, indices, fixed2, config)
+    for iter in range(0, num_steps):
+        nodes2 = sim2.step()
+    print(nodes2[-1])
+    plot_nodes(nodes2)
+
+def test_cantilever_static():
+    config = {
+        "young": 66000,
+        "poisson": 0.45,
+        "simtype": 0,
+        "substeps": 10,
+        'maxiter': 100,
+    }
+
+    # load the box
+    verts, indices = read_tetfile('box2.tet')
+    fixed2 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    num_steps = 10
+
+    # C++ simulator
+    sim2 = psf.NonlinearFEM(indices, verts, fixed2, config)
+    nodes2 = sim2.get_nodes()
+    sim2.step()
+    nodes2 = sim2.get_nodes()
+    sim2.save_to_vtk('cantilever.vtk')
+    print(nodes2[-1])
+    plot_nodes(nodes2)
+
+    # Python simulator
+    sim2 = Simulator(verts, indices, fixed2, config)
+    nodes2 = sim2.step_static()
+    print(nodes2[-1])
+    plot_nodes(nodes2)
+
+
+def test_hammerbot():
+    config = {
+        "young": 66000,
+        "poisson": 0.45,
+        "simtype": 0,
+        "substeps": 10,
+        'maxiter': 100,
+    }
+
+    # create the simulator from file
+    fem = psf.NonlinearFEM('hammerbot.xml')
+    fem.step()
+    nodes = fem.get_nodes()
+    fem.save_to_vtk("hammerbot.vtk")
+    
+    #N, T, F = psf.load_from_xml('hammerbot.xml')
+    #print(N)
+    #hammerbot = Simulator(N, T.flatten(), F, config)
+    #nodes4 = hammerbot.step()
+    #plot_nodes(nodes4)
+
+test_cantilever_static()
 
 # explicit integration
 #fem = psf.NonlinearFEM('hammerbot_explicit.xml')

PyTester/Simulator.py
@@ -1,24 +1,31 @@
 import numpy as np
 import math
+from scipy.optimize import minimize
 
 class Simulator:
 
-    dt = 0.016
     gravity = np.array([0, -9.8, 0])
     young = 66000
-    poisson = 0.4
+    poisson = 0.45
     density = 1070
 
     def __init__(self,
                 nodes,
                 tets,
-                fixed_nodes
+                fixed_nodes,
+                config
         ):
         """
         @param nodes: Array of node positions
         @param tets: Array of tetrahedra indices
         @param fixed_nodes: Array of indices indicating fixed nodes
+        @param config: Dictionary containing configuration
         """        
+        self.substeps = 1
+        if "substeps" in config:
+            self.substeps = config["substeps"]
+        self.dt = 0.016 / self.substeps
+            
         # init members
         self.nodes = nodes
         numTets = math.ceil(tets.shape[0] / 4)
@@ -29,7 +36,7 @@ class Simulator:
         self.volumes = []
         self.jacobians = []
         for e, tet in enumerate(self.tets):            
-            # get the 4 points
+            # get the 4 points            
             x0 = self.nodes[tet[0]]
             x1 = self.nodes[tet[1]]
             x2 = self.nodes[tet[2]]
@@ -59,7 +66,9 @@ class Simulator:
         self.la = self.young * self.poisson / (1 + self.poisson) / (1 - 2 * self.poisson)        
         
     def step(self):
-        return self.step_explicit()
+        for iter in range(0, self.substeps):
+            self.step_explicit()
+        return self.nodes
         
     def step_explicit(self):  
         # compute forces
@@ -70,19 +79,45 @@ class Simulator:
         return self.nodes
     
     def step_static(self):
-        # gradient descent
-        numIters = 100
-        alpha = 1e-3
+        #return self.solve_grad_desc()
+        return self.solve_scipy()
+
+    def solve_scipy(self):
+        x0 = self.nodes[self.free].flatten()
+        sol = minimize(self.energy_objective, x0, method='Newton-CG', jac=self.jacobian)
+        print(sol.message)
+        self.nodes[self.free] = np.reshape(sol.x, (int(sol.x.shape[0] / 3), 3))
+        return self.nodes
+        
+    def energy_objective(self, x):
+        self.nodes[self.free] = np.reshape(x, (int(x.shape[0] / 3), 3))
+        energy = self.compute_energy()
+        #print(energy)
+        return energy
+    
+    def jacobian(self, x):
+        self.nodes[self.free] = np.reshape(x, (int(x.shape[0] / 3), 3))
+        g = -self.compute_gradients()
+        return g[self.free].flatten()
+        
+    def solve_grad_desc(self, alpha = 1e-2, numIters = 1000, rel_tol=1e-9):
+        # gradient descent   
         energy = self.compute_energy()
         for iter in range(0, numIters):
             f = self.compute_gradients()            
             self.nodes[self.free] = self.nodes[self.free] + alpha * f[self.free]
-            old_energy = energy;
+            #print(energy)
+            old_energy = energy;            
             energy = self.compute_energy()
             rel_err = abs((energy - old_energy) / energy)
-            if rel_err < 1e-15:
+            #print(rel_err)
+            if rel_err < rel_tol:
                 print('Converged after {0} iterations'.format(iter+1))
                 break
+            if energy > old_energy:
+                print('Not converging')
+                break
+        print(energy)
         return self.nodes
 
     # returns the elstic forces
@@ -118,6 +153,8 @@ class Simulator:
         for e, tet in enumerate(self.tets):
             F = self.compute_deformation_gradient(e)
             J = np.linalg.det(F)
+            if J < 0:
+                return float('inf')
             C = np.matmul(np.transpose(F), F)
             I1 = np.trace(C)
             logJ = math.log(J)