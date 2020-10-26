b2c60ef813d8e36a59f1016b4eeea3e369d1c188
Mihai Francu <mishu.francu@gmail.com>
2020-10-26 16:47:41 +0100

    Vectorized the torch simulator

--- PyTester/TestTorchSim.py
+++ PyTester/TestTorchSim.py
@@ -1,6 +1,9 @@
 from TorchSimulator import TorchSimulator, TorchSimulatorFast
 from utils import read_tetfile
 import time
+import torch
+
+torch.set_printoptions(precision=10)
 
 config = {
     "young": 66000,
@@ -13,19 +16,22 @@ config = {
 # load the box
 verts, indices = read_tetfile('box2.tet')
 fixed2 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-num_steps = 10
+num_steps = 60000
+device = 'cpu'
 
 # Python torch simulator
-#ts_slow_begin = time.time()
-#sim2 = TorchSimulator(verts, indices, fixed2, config)
-#sim2.solve_grad_desc(1e-5, 100)
-#ts_slow_end = time.time()
-#print(sim2.nodes[-1])
-#print("Time (slow):", ts_slow_end - ts_slow_begin)
-
-ts_fast_begin = time.time()
-sim2 = TorchSimulatorFast(verts, indices, fixed2, config)
-sim2.solve_grad_desc(1e-5, 100)
-ts_fast_end = time.time()
+ts_slow_begin = time.time()
+sim2 = TorchSimulator(verts, indices, fixed2, config, device)
+# for iter in range(0, num_steps):
+#     sim2.step_explicit()
+sim2.solve_grad_desc(2e-5, num_steps)
+ts_slow_end = time.time()
 print(sim2.nodes[-1])
-print("Time (fast):", ts_fast_end - ts_fast_begin)
+print("Time (slow):", ts_slow_end - ts_slow_begin)
+
+# ts_fast_begin = time.time()
+# sim2 = TorchSimulatorFast(verts, indices, fixed2, config, device)
+# sim2.solve_grad_desc(1e-5, 100)
+# ts_fast_end = time.time()
+# print(sim2.nodes[-1])
+# print("Time (fast):", ts_fast_end - ts_fast_begin)
--- PyTester/TorchSimulator.py
+++ PyTester/TorchSimulator.py
@@ -2,11 +2,13 @@ import torch
 import numpy as np
 import math
 
+from torch_scatter import scatter_add
+
 class TorchSimulator:
 
     young = 66000
     poisson = 0.45
-    density = 1070
+    density = 1070    
 
     def __init__(self,
                 nodes,
@@ -22,7 +24,7 @@ class TorchSimulator:
         @param config: Dictionary containing configuration
         @param device: CPU or GPU device for torch
         """
-        print(torch.cuda.is_available())
+        print("CUDA support: {}".format(torch.cuda.is_available()))
 
         # init members
         self.substeps = 1
@@ -36,37 +38,36 @@ class TorchSimulator:
         # init FEM configuration
         self.nodes = torch.from_numpy(nodes).float().to(self.device)
         numTets = math.ceil(tets.shape[0] / 4)
+        numNodes = nodes.shape[0]
         self.tets = tets.reshape((numTets, 4))
-        self.masses = torch.zeros((nodes.shape[0], 1), dtype=torch.float, device=self.device)
+        self.masses = torch.zeros(numNodes, dtype=torch.float, device=self.device)
         self.vel = torch.zeros(nodes.shape, dtype=torch.float, device=self.device)
+        # build shape matrices (batch)
+        self.indices = torch.from_numpy(tets).to(self.device)
+        self.idx0 = self.indices[0::4]
+        self.idx1 = self.indices[1::4]
+        self.idx2 = self.indices[2::4]
+        self.idx3 = self.indices[3::4]
+        col1 = self.nodes[self.idx1] - self.nodes[self.idx0]
+        col2 = self.nodes[self.idx2] - self.nodes[self.idx0]
+        col3 = self.nodes[self.idx3] - self.nodes[self.idx0]
+        shape_mats = torch.empty(numTets, 3, 3, device=self.device)
+        shape_mats[:,:,0] = col1
+        shape_mats[:,:,1] = col2
+        shape_mats[:,:,2] = col3
         # compute volumes and masses
-        self.volumes = []
-        self.jacobians = []
-        for e, tet in enumerate(self.tets):
-            # get the 4 points
-            x0 = self.nodes[tet[0]]
-            x1 = self.nodes[tet[1]]
-            x2 = self.nodes[tet[2]]
-            x3 = self.nodes[tet[3]]
-            # build the columns of the matrix
-            d1 = x1 - x0
-            d2 = x2 - x0
-            d3 = x3 - x0
-            # store the volume and the tranpose inverse
-            D = torch.cat([d1.view(-1, *d1.shape), d2.view(-1, *d2.shape), d3.view(-1, *d3.shape)], dim=0).T
-            vol = torch.det(D) / 6.0
-            self.volumes.append(vol)
-            Dinv = torch.inverse(D)
-            self.jacobians.append(Dinv.T)
-            # compute the element mass
-            mass = 0.25 * vol * self.density
-            self.masses[tet[0]] += mass;
-            self.masses[tet[1]] += mass;
-            self.masses[tet[2]] += mass;
-            self.masses[tet[3]] += mass;
+        self.vols = torch.det(shape_mats) / 6
+        self.inv_mats = torch.inverse(shape_mats)        
+        elem_masses = self.vols * self.density / 4
+        # average the node masses
+        scatter_add(elem_masses, self.idx0, -1, self.masses)
+        scatter_add(elem_masses, self.idx1, -1, self.masses)
+        scatter_add(elem_masses, self.idx2, -1, self.masses)
+        scatter_add(elem_masses, self.idx3, -1, self.masses)
         # compute inverse masses
         self.inv_masses = torch.zeros_like(self.masses, dtype=torch.float, device=self.device)
-        self.free = np.setdiff1d(range(0, nodes.shape[0]), fixed_nodes)
+        free_idx = np.setdiff1d(range(0, nodes.shape[0]), fixed_nodes)
+        self.free = torch.from_numpy(free_idx).to(self.device)
         self.inv_masses[self.free] = 1.0 / self.masses[self.free]
         # init params
         self.mu = 0.5 * self.young / (1 + self.poisson)
@@ -78,6 +79,7 @@ class TorchSimulator:
 
     def step_explicit(self):
         # compute forces
+        self.compute_def_grads()
         f = self.compute_forces()
         # Semi-implicit (symplectic) Euler integration
         self.vel[self.free] = self.vel[self.free] + self.dt * (self.gravity + self.inv_masses[self.free] * f[self.free])
@@ -86,57 +88,84 @@ class TorchSimulator:
     # gradient descent static solver
     def solve_grad_desc(self, alpha = 1e-2, numIters = 1000, rel_tol=1e-9):
         # gradient descent
+        self.compute_def_grads()
+        f = self.compute_gradients()
         energy = self.compute_energy()
-        for iter in range(0, numIters):
-            f = self.compute_gradients()
+        for iter in range(0, numIters):            
             self.nodes[self.free] = self.nodes[self.free] + alpha * f[self.free]
-            #print(energy)
-            old_energy = energy;
+            # update gradients and energy
+            self.compute_def_grads()
+            f = self.compute_gradients()
+            old_energy = energy
             energy = self.compute_energy()
+            #print(energy)            
             rel_err = abs((energy - old_energy) / energy)
             #print(rel_err)
-            if rel_err < rel_tol:
-                print('Converged after {0} iterations'.format(iter+1))
-                break
+            # if rel_err < rel_tol:
+            #     print('Converged after {0} iterations'.format(iter+1))
+            #     break
             if energy > old_energy:
                 print('Not converging')
                 break
         print(energy)
+        print(rel_err)
 
     # returns the elastic forces
+    def compute_def_grads(self):
+        # build shape matrices (batch)
+        idx0 = self.indices[0::4]
+        idx1 = self.indices[1::4]
+        idx2 = self.indices[2::4]
+        idx3 = self.indices[3::4]
+        col1 = self.nodes[idx1] - self.nodes[idx0]
+        col2 = self.nodes[idx2] - self.nodes[idx0]
+        col3 = self.nodes[idx3] - self.nodes[idx0]
+        numTets = numTets = math.ceil(self.indices.shape[0] / 4)
+        shape_mats = torch.empty(numTets, 3, 3, device=self.device)
+        shape_mats[:,:,0] = col1
+        shape_mats[:,:,1] = col2
+        shape_mats[:,:,2] = col3
+        #compute deformation gradients (batch)
+        self.F = torch.bmm(shape_mats, self.inv_mats)
+        self.Finv = torch.inverse(self.F)        
+        self.J = torch.det(self.F)
+        self.logJ = torch.log(self.J)
+
     def compute_forces(self):
+        # compute the stress tensors (first Piola-Kirchoff)        
+        Fit = torch.transpose(self.Finv, 1, 2)
+        P = self.mu * (self.F - Fit) + self.la * self.logJ.view(-1,1,1) * Fit
+        # compute force matrices
+        Pvol = -self.vols.view(-1,1,1) * P
+        force_mats = torch.bmm(Pvol, torch.transpose(self.inv_mats, 1, 2))
+        
+        #numNodes = self.nodes.shape[0]
         f = torch.zeros(self.nodes.shape, dtype=torch.float, device=self.device)
-        for e, tet in enumerate(self.tets):
-            P = self.compute_stress(e)
-            H = -self.volumes[e] * torch.matmul(P, self.jacobians[e])
-            for j in range(1,4):
-                force = H[:, j - 1]
-                f[tet[j]] += force
-                f[tet[0]] -= force
+        f0 = -(force_mats[:,:,0] + force_mats[:,:,1] + force_mats[:,:,2])
+        scatter_add(f0, self.idx0, 0, f)
+        scatter_add(force_mats[:,:,0], self.idx1, 0, f)
+        scatter_add(force_mats[:,:,1], self.idx2, 0, f)
+        scatter_add(force_mats[:,:,2], self.idx3, 0, f)
         return f
 
     # returns the negative gradients
     def compute_gradients(self):
         f = self.compute_forces()
-        # TODO: optimize
-        for i, v in enumerate(self.nodes):
-            if self.inv_masses[i] != 0:
-                f[i] = f[i] + self.masses[i] * self.gravity
+        f[self.free] = f[self.free] + self.masses[self.free].unsqueeze(1) * self.gravity
         return f
 
     def compute_energy(self):
         energy = self.compute_elastic_energy()
-        # TODO: dot product between nodes and gravity weighed by masses
-        for i, v in enumerate(self.nodes):
-            if self.inv_masses[i] != 0:
-                energy -= self.masses[i, 0] * self.gravity[1] * self.nodes[i, 1]
-        return energy
+        G = self.masses * torch.matmul(self.nodes, self.gravity)
+        return energy - G[self.free].sum(-1) # drop the self.free
 
-    def compute_elastic_energy(self):
-        energy = 0
-        for e, tet in enumerate(self.tets):
-            energy += self.compute_elem_energy(e)
-        return energy
+    def compute_elastic_energy(self):        
+        # compute the per element energies
+        C = torch.bmm(torch.transpose(self.F, 1, 2), self.F)
+        I1 = torch.diagonal(C, dim1=1, dim2=2).sum(-1) # the trace of C
+        E = 0.5 * self.mu * (I1 - 3) - self.mu * self.logJ + 0.5 * self.la * self.logJ * self.logJ
+        elemE = self.vols * E
+        return elemE.sum(-1)
 
     def compute_elem_energy(self, e):
         F = self.compute_deformation_gradient(e)
@@ -146,8 +175,8 @@ class TorchSimulator:
         C = torch.matmul(F.T, F)
         I1 = torch.trace(C)
         logJ = math.log(J)
-        E = 0.5 * self.mu * (I1 - 3) - self.mu * logJ + 0.5 * self.la * logJ * logJ;
-        return self.volumes[e] * E
+        E = 0.5 * self.mu * (I1 - 3) - self.mu * logJ + 0.5 * self.la * logJ * logJ
+        return self.vols[e] * E
 
     def compute_stress(self, e):
         F = self.compute_deformation_gradient(e)
@@ -155,7 +184,9 @@ class TorchSimulator:
         Fi = torch.inverse(F)
         Fit = Fi.T
         J = torch.det(F)
-        P = self.mu * (F - Fit) + self.la * math.log(J) * Fit;
+        P = self.mu * (F - Fit) + self.la * math.log(J) * Fit
+        if e == 0:
+            print(P)
         return P
 
     def compute_deformation_gradient(self, e):
@@ -171,7 +202,7 @@ class TorchSimulator:
         d3 = x3 - x0
         # compute the deformation gradient
         D = torch.cat([d1.view(-1, *d1.shape), d2.view(-1, *d2.shape), d3.view(-1, *d3.shape)], dim=0).T
-        F = torch.matmul(D, self.jacobians[e].T)
+        F = torch.matmul(D, self.inv_mats[e])
         return F
 
 
